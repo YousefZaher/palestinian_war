22cd57539c2cb3b11251bc8362ec5131b91f499e
Yousef Zaher <s12325147@stu.najah.edu>
2025-05-27 12:27:03 +0300

    improving the over-look and visiuals of the project

--- src/gui/MainFrameZ.java
+++ src/gui/MainFrameZ.java
@@ -32,6 +32,19 @@ public class MainFrameZ extends JFrame {
 
 
     public MainFrameZ(final ArrayList<RegionData> initialRegions){
+        
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
         super("Palestinian War Analyzer");
         JPanel header= new JPanel();
         header.setBackground(Color.RED);
